f7fc2ae6115513d7107dc8e1dfd0943205519ff8
Chandana19-cell <77376010+Chandana19-cell@users.noreply.github.com>
2023-10-07 14:29:48 +0530

    Add files via upload
    done

---
+++ callbackHell.js
@@ -0,0 +1,38 @@
+function ten() {
+    setTimeout(() => {
+        document.getElementById("demo").innerHTML= "10";
+        setTimeout(() => {
+            document.getElementById("demo").innerHTML="9";
+            setTimeout(() => {
+                document.getElementById("demo").innerHTML="8";
+                setTimeout(() => {
+                    document.getElementById("demo").innerHTML="7";
+                    setTimeout(() => {
+                        document.getElementById("demo").innerHTML="6";
+                        setTimeout(() => {
+                            document.getElementById("demo").innerHTML="5";
+                            setTimeout(() => {
+                                document.getElementById("demo").innerHTML="4";
+                                setTimeout(() => {
+                                    document.getElementById("demo").innerHTML="3";
+                                    setTimeout(() => {
+                                        document.getElementById("demo").innerHTML="2";
+                                        setTimeout(() => {
+                                            document.getElementById("demo").innerHTML="1";
+                                            setTimeout(() => {
+                                                document.getElementById("demo").innerHTML="Happy Independance Day";
+                                            }, 2000)
+
+                                        }, 2000)
+                                    }, 2000)
+                                }, 2000)
+                            }, 2000)
+                        }, 2000)
+                    }, 2000)
+                }, 2000)
+            }, 2000)
+        }, 2000)
+    }, 1000)
+}
+
+ten();
